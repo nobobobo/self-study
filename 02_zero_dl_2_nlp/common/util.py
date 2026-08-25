8875028abc603babe070c49f6b5ac4a9895bb659
Noboru Hayashi <nhayashi@bu.edu>
2026-08-25 13:23:16 +0900

    ch4

--- 02_zero_dl_2_nlp/common/util.py
+++ 02_zero_dl_2_nlp/common/util.py
@@ -1,5 +1,22 @@
 import numpy as np
 
+def to_cpu(x):
+    import numpy
+    if type(x) == numpy.ndarray:
+        return x
+    return np.asnumpy(x)
+
+
+def to_gpu(x):
+    try:
+        import cupy
+    except ImportError:
+        return np.asarray(x)
+
+    if type(x) == cupy.ndarray:
+        return x
+    return cupy.asarray(x)
+
 def preprocess(text):
     text = text.lower()
     text = text.replace('.', ' .')
@@ -18,6 +35,42 @@ def preprocess(text):
 
     return corpus, word_to_id, id_to_word
 
+def create_contexts_target(corpus, window_size=1):
+    target = corpus[window_size:-window_size]
+    contexts = []
+
+    for idx in range(window_size, len(corpus) - window_size):
+        cs = []
+        for t in range(-window_size, window_size + 1):
+            if t == 0:
+                continue
+            cs.append(corpus[idx + t])
+        contexts.append(cs)
+
+    return np.array(contexts), np.array(target)
+
+def convert_one_hot(corpus, vocab_size):
+    '''one-hot表現への変換
+
+    :param corpus: 単語IDのリスト（1次元もしくは2次元のNumPy配列）
+    :param vocab_size: 語彙数
+    :return: one-hot表現（2次元もしくは3次元のNumPy配列）
+    '''
+    N = corpus.shape[0]
+
+    if corpus.ndim == 1:
+        one_hot = np.zeros((N, vocab_size), dtype=np.int32)
+        for idx, word_id in enumerate(corpus):
+            one_hot[idx, word_id] = 1
+
+    elif corpus.ndim == 2:
+        C = corpus.shape[1]
+        one_hot = np.zeros((N, C, vocab_size), dtype=np.int32)
+        for idx_0, word_ids in enumerate(corpus):
+            for idx_1, word_id in enumerate(word_ids):
+                one_hot[idx_0, idx_1, word_id] = 1
+
+    return one_hot
 
 def create_co_matrix(corpus, vocab_size, window_size=1):
     corpus_size = len(corpus)
@@ -87,4 +140,15 @@ def ppmi(C, verbose=False, eps=1e-8):
                     print('%.1f%% done' % (100 * cnt / total))
 
     return M
-                
+
+
+def clip_grads(grads, max_norm):
+    total_norm = 0
+    for grad in grads:
+        total_norm += np.sum(grad ** 2)
+    total_norm = np.sqrt(total_norm)
+
+    rate = max_norm / (total_norm + 1e-6)
+    if rate < 1:
+        for grad in grads:
+            grad *= rate
